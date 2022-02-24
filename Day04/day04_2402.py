c13b9bc8ac84c2613d07c8f80873c31d3eb642b0
anjaligarala <anjaligarala15@gmail.com>
2022-02-24 09:43:49 +0530

    Day 4 Learning and Pratice Lists

---
+++ Day04/day04_2402.py
@@ -0,0 +1,12 @@
+name = input("Enter Name - ")
+
+for i in (range(len(name))):
+    if i%2 == 0:
+        print("This is even value -" + name[i])
+    else:
+        print("This is odd value -" + name[i])
+
+        
+
+     
+
