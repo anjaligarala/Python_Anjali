d60c05bdcaf83825361323e6225e1979a37afecd
anjaligarala <anjaligarala15@gmail.com>
2022-02-28 10:14:50 +0530

    Day 6 pratice list and Hacker Rank

---
+++ Day06/day06_2802.py
@@ -0,0 +1,12 @@
+user_input = [elem for elem in input().split()]
+
+#split string 
+#str_convert = user_input.split(" ")
+#print(str_convert)
+
+#split one string in list 
+#print(list(user_input))
+
+
+#str_convert = user_input.split(" ")
+#print("".join(str_convert))
