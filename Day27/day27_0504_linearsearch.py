9ff4665f94bd9c2edbcf8b9f43c5ceb9b5628125
anjaligarala <anjaligarala15@gmail.com>
2022-04-07 07:50:36 +0530

    Day 28 Pratice

--- Day27/day27_0504_linearsearch.py
+++ Day27/day27_0504_linearsearch.py
@@ -1,8 +1,18 @@
 # Write a program to implement liner search and find the position of the no
+import time
+import datetime
 
-list01 = [4, 2, 7, 1, 8, 3, 6]
+list01 = list(range(1000))
 user_input = int(input())
+start_time = datetime.datetime.now()
 
-for ele in range(list01):
+for ele in range(len(list01)):
     if list01[ele] == user_input:
-        print(ele)
+        print(ele, user_input)
+        end_time = datetime.datetime.now()
+        print("Your algorithm took " + str((end_time - start_time).total_seconds()))
+        exit()
+
+print("Element not found")
+end_time = datetime.datetime.now()
+print("Your algorithm took " + str((end_time - start_time).total_seconds()))
